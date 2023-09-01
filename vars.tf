0931b1d40fcd44b1548e9ee9d741fd1d6dc322bf
vyshuvenu <vyshuv1289@gmail.com>
2023-09-01 12:04:28 +0530

    new commit

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "env" {}
+variable "tags" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "elasticache_type" {}
+variable "family" {}
+variable "port" {}
+variable "engine" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "engine_version" {}
